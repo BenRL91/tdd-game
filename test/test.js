bc9d05d23ad1f9a837adb8c5b7b2a46fb946fa5c
Ben Lee <leeohsunshine@gmail.com>
2016-07-14 09:27:33 -0400

    Initial Commit.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+// Import Chai
+import chai from 'chai';
+
+// Import Any Files to Test
+
+// Set Chai Constants
+const expect = chai.expect;
+const should = chai.should();
+const assert = chai.assert;
